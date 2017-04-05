b41aeafd1f520b6deb8eff5ff2c8e4f025828ae4
quaker_chung <quaker_chung@htc.com>
2017-04-05 12:59:33 +0800

    fix lot.  Can buy stamina.

--- trust.lua
+++ trust.lua
@@ -23,9 +23,15 @@ addTextView("執行次數：")addEditNumber("CLEAR_LIMIT", 999)newRow()
 --addTextView("體力不足時等待 (分)：")addEditNumber("WAIT_TIME", 3)newRow()
 addTextView("選擇關卡：")newRow()
 addRadioGroup("QUEST", 1)addRadioButton("入口", 1)addRadioButton("最深處", 2)newRow()
+FRIEND = false
 addCheckBox("FRIEND", "選擇朋友", false)newRow()
+BUY = false
 addCheckBox("BUY", "使用寶石回復體力 ", false)addEditNumber("BUY_LOOP", 2)addTextView(" 回")newRow()
 dialogShow("Trust Master Maker".." - "..X.." × "..Y)
+
+if (BUY) then
+    toast("Will buy stamina")
+end
 
 X12 = X / 2
 X14 = X / 4
@@ -64,7 +70,7 @@ rightLeft  = Region(X12,0, X34, Y)
 rightRight = Region(X34,0, X, Y)
 
 -- 1/6
-middleRight = Region(X13, Y12, X23, Y)
+middleRight = Region(X12, Y13, X, Y23)
 
 -- 1/8
 upperUpperLeft  = Region(0, 0, X12, Y14)
@@ -104,22 +110,21 @@ switch = {
                     STEP = 2
                 end
             else
-                if (BUY and middleRight:existsClick("Use_Gem.png")) then
+                if (BUY and BUY_LOOP > 0 and middleRight:existsClick("Use_Gem.png")) then
                     lowerUpperRight:existsClick("Buy_Yes.png")
+                    existsClick("09_Return.png") -- leave quest and redo step 1
                     print("使用寶石回復體力")
-                    break;
-                else
-                    if (lowerUpperMiddle:existsClick("Stamina_Back.png")) then
-                        toast('體力不足，等待中...')
-                        print("Wait for stamina")
-                        setScanInterval(10)
-                        wait(30)
-                        setScanInterval(2)
-                    end
+                    BUY_LOOP = BUY_LOOP - 1
+                elseif (lowerUpperMiddle:existsClick("Stamina_Back.png")) then
+                    toast('體力不足，等待中...')
+                    print("Wait for stamina")
+                    setScanInterval(10)
+                    wait(30)
+                    setScanInterval(2)
                 end
             end
         else
-            toast('找不到 "土之神殿。入口 "')
+            toast('找不到關卡')
             existsClick("06_Next.png")
             existsClick("09_Return.png")
             STEP = 1
