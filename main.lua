10ff011f2f8e429bef39d33153e5d881a717ae39
quaker_chung <quaker_chung@htc.com>
2017-04-13 18:19:52 +0800

    Add license. Separate out a watchdog.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,20 @@
--- Edit by Quaker NTj
+-- Copyright © 2017 Quaker NTj <quakerntj@hotmail.com>
+
+--[[
+    This script is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This script is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
 WORK_DIR = scriptPath()
 package.path = package.path .. ";" .. WORK_DIR .. '?.lua'
 
--- trust.lua
+++ trust.lua
@@ -1,34 +1,19 @@
--- Edit by Quaker NTj
+-- Copyright © 2017 Quaker NTj <quakerntj@hotmail.com>
 
-WatchDog = {}
-WatchDog.__index = WatchDog
+--[[
+    This script is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-setmetatable(WatchDog, {
-	__call = function (cls, ...)
-		return cls.new(...)
-	end,
-})
+    This script is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-function WatchDog.new(timeout, obj, bark)
-	local self = setmetatable({}, WatchDog)
-	self.timeout = timeout
-	self.timer = Timer()
-	self.timer:set()
-	self.obj = obj
-	self.bark = bark -- callback function
-	return self
-end
-
-function WatchDog:touch()
-	self.timer:set()
-	self.lastTouch = self.timer:check()
-end
-
-function WatchDog:awake()
-	if (self.timer:check() > self.timeout) then
-		self.bark(self.obj, self)  -- User should touch the dog themself.
-	end
-end
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
 
 TrustManager = {}
 TrustManager.__index = TrustManager
